82f004e5612e12fe1c2051ab94e72ed4dccff808
Giu Sousa <sou.giuliana@outlook.com>
2021-10-27 07:41:43 +0000

    implementação pypi e serializers

--- api/pypi.py
+++ api/pypi.py
@@ -1,20 +1,30 @@
 import requests
+import json
 
 # Referências sobre o uso do requests:
-#
+
 # Fazendo requisições:
 # https://docs.python-requests.org/en/master/user/quickstart/#make-a-request
 # Usando JSON retornado:
 # https://docs.python-requests.org/en/master/user/quickstart/#json-response-content
 
+# Checar se o pacote existe, considerando ou não a versão
 def version_exists(package_name, version):
-    # TODO
-    # Fazer requisição na API do PyPI para checar se a versão existe
-    return False
+    dados_pacote = requests.get(f"https://pypi.org/pypi/{package_name}/{version}/json")
+    if dados_pacote:
+        return True
+    else:
+        return False
 
 
 def latest_version(package_name):
     # TODO
     # Fazer requisição na API do PyPI para descobrir a última versão
     # de um pacote. Retornar None se o pacote não existir.
-    return "?"
+
+    dados_pacote = requests.get(f"https://pypi.odados_pacoteg/pypi/{package_name}/json")
+    if dados_pacote.status_code == 404:
+        return "None"
+    else:
+        dados_pacote = dados_pacote.json()
+        return list(dados_pacote['releases'].keys())[-1]
--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
-
+from django.core.exceptions import ValidationError
 from .models import PackageRelease, Project
 from .pypi import version_exists, latest_version
+import json
+from rest_framework.renderers import JSONRenderer
 
 
 class PackageSerializer(serializers.ModelSerializer):
@@ -10,14 +12,38 @@ class PackageSerializer(serializers.ModelSerializer):
         fields = ["name", "version"]
         extra_kwargs = {"version": {"required": False}}
 
+    # Validar o pacote, checar se ele existe na versão especificada.
     def validate(self, data):
-        # TODO
-        # Validar o pacote, checar se ele existe na versão especificada.
-        # Buscar a última versão caso ela não seja especificada pelo usuário.
-        # Subir a exceção `serializers.ValidationError()` se o pacote não
-        # for válido.
-        return data
 
+        especificada = False
+        
+        # Checar se a versão foi especificada
+        for chave in data:
+            if chave == 'version':
+                versao_pacote = data[chave]
+                especificada = True
+        
+        # Checar se número da versão é válido
+        if especificada:
+            existente = version_exists(data["name"], versao_pacote)
+            if existente == True:
+                return data
+            else:
+                raise serializers.ValidationError({"error": "One or more packages doesn't exist"})
+        
+        # Se a versão não for especificada, procurar a última versão
+        else:
+            ultima_versao = latest_version(data["name"])
+            
+            # Subir a exceção `serializers.ValidationError()` se o pacote não
+            # for válido
+            if ultima_versao == "None":
+                raise serializers.ValidationError({"error": "One or more packages doesn't exist"})
+            
+            # Retornar os dados atualizados e completos
+            else:
+                data['version'] = ultima_versao
+                return data
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
@@ -27,11 +53,20 @@ class ProjectSerializer(serializers.ModelSerializer):
     packages = PackageSerializer(many=True)
 
     def create(self, validated_data):
-        # TODO
-        # Salvar o projeto e seus pacotes associados.
-        #
         # Algumas referência para uso de models do Django:
         # - https://docs.djangoproject.com/en/3.2/topics/db/models/
         # - https://www.django-rest-framework.org/api-guide/serializers/#saving-instances
+        
         packages = validated_data["packages"]
-        return Project(name=validated_data["name"])
+
+        # Definir nome do projeto (qualquer nome)
+        projeto = Project.objects.create(name=validated_data["name"])
+
+        # Iterar por todos pacotes
+        for i in range(len(packages)):
+            package = PackageRelease.objects.create(name=packages[i]['name'], version=packages[i]['version'], project=projeto)
+
+        # Salvar o projeto e seus pacotes associados.
+        projeto.save()
+
+        return projeto
